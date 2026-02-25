cbb601321fa3cfdd81dc4096e24217c524046a1c
Philasande Bhani <85154876+pbhaninaa@users.noreply.github.com>
2026-02-25 23:06:47 +0200

    .

--- WebApp/vite.config.js
+++ WebApp/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       vue(),
       vueDevTools(),
-      basicSsl(),
+      mkcert(),
     ],
     resolve: {
       alias: {
